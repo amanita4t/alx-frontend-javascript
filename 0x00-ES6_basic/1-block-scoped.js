e2546060653248acfbf6e30df487d0a4ff77433b
amanita4t <amanueltekalign4@gmail.com>
2023-09-26 18:25:14 +0300

    test

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
     var task2 = true;
   
     if (trueOrFalse) {
-      let task = true;   // Use let instead of var
-      let task2 = false; // Use let instead of var
+      let task = true;   // I Use let instead of var
+      let task2 = false; // I Use let instead of var
     }
   
     return [task, task2];
